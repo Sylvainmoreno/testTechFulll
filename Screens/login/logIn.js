e6b12a2316404ea75675803d801f5bc2b0d8099f
Sylvain Moreno <>
2021-08-06 18:00:57 +0200

    formik && yup added

--- Screens/login/logIn.js
+++ Screens/login/logIn.js
@@ -1,15 +1,77 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Image, Text, StyleSheet, TouchableWithoutFeedback, Keyboard } from "react-native";
+import { Formik } from 'formik';
+import * as yup from 'yup'
+import IEDFormInput from '../../components/IEDFormInput/IEDFormInput'
+import IEDFormButton from "../../components/IEDFormButton/IEDFormButton";
+
+
+const imgLogo = require("fulll/assets/images/ied-logo.png");
+
+const loginValidation = yup.object().shape({
+    email: yup.string().email("Please enter valid email").required('Email Address is Required'),
+    password: yup.string().matches(/(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}/, "Password must have at least 1 special character.").min(6, ({ min }) => `Password must be at least ${min} characters`).required('Password is required'),
+})
 
 
 export default function Login({ navigation: { navigate }}) {
     return (
-        <View>
-            <Text>"ALOOOOOOOLLLL"</Text>
-            <Button
-                title="Go to Details"
-                onPress={() => navigate('HomeStack')}
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style= {{flex: 1}}>
+            <View style={styles.logoContainer}>
+            <Image 
+                  style={styles.logo}
+                  source={imgLogo}
             />
-        </View>
+            </View>
+                <Formik
+                    validationSchema={loginValidation}
+        initialValues={{ email: '', password: '' }}
+        onSubmit={values => console.log(values)}
+        >
+        {({ handleChange, handleSubmit, values, errors, touched }) => (
+                        <View>
+                                                {touched.email && errors.email && (
+                      <Text style={styles.textError}>{errors.email}</Text>
+                    )}
+        <IEDFormInput
+            autoCompleteType="email"
+            secureTextEntry={false}
+                                autoCorrect={false}
+                                returnKeyType="next"
+              keyboardType="email-address"
+              handleChangedText={handleChange('email')}
+              value={values.email}s
+            />
+        <IEDFormInput
+            autoCompleteType="password"
+            secureTextEntry={true}
+                                autoCorrect={false}
+                                returnKeyType="done"
+            keyboardType="default"
+              handleChangedText={handleChange('password')}
+              placeholderText= "Please enter your password"
+              value={values.password}
+            />
+                            <IEDFormButton onSubmit={handleSubmit} TextButton="Submit"/>
+          </View>
+        )}
+        </Formik>
+            </View>
+            </TouchableWithoutFeedback>
     )
 }
+
+const styles = StyleSheet.create({
+    logoContainer: {
+        marginTop: "20%",
+        height: "25%",
+        marginBottom: 100
+    },
+    logo: {
+        alignSelf: "center",
+        width: "100%",
+        height: "100%",
+        resizeMode: "contain"
+    },
+  })
---
+++ components/IEDFormButton/IEDFormButton.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+    TouchableOpacity,
+    StyleSheet,
+    Text } from "react-native";
+import propTypes from 'prop-types';
+
+
+const IEDFormButton = (props) => {
+
+    const { TextButton, onSubmit } = props;
+
+    return (
+      
+        <TouchableOpacity style={buttonStyles.buttonContainer} onPress={onSubmit}>
+            <Text style={buttonStyles.textStyle}>{TextButton}</Text>
+            </TouchableOpacity>
+    
+    )
+
+}
+
+const buttonStyles = StyleSheet.create({
+    buttonContainer: {
+        width: "80%",
+        height: "20%",
+        alignSelf: "center",
+        marginTop: "20%",
+        borderWidth: 1,
+        borderRadius: 50,
+        borderColor: "#86bc25",
+        backgroundColor: "#86bc25"
+    },
+    textStyle: {
+        flex: 1,
+        textAlign: "center",
+        textAlignVertical: "center",
+        color: "white",
+        fontSize: 20,
+        fontWeight: "bold"
+    }
+})
+
+export default IEDFormButton;
